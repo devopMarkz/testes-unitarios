857189942cc81f03e4bfd8b709171af97772fff8
devopMarkz <marcosdev2002@gmail.com>
2025-02-26 10:00:52 -0300

    first commit

---
+++ src/test/java/com/erudio/app_test/AppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erudio.app_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
